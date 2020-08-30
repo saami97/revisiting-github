83ed51539707a80bf82a842b293df6f26805eca3
Amir Sayed <amirsayed54@gmail.com>
2020-08-30 10:13:14 +0530

    now master is truly merged with test-branch

--- hey.py
+++ hey.py
@@ -1,8 +1,3 @@
 
 
-<<<<<<< HEAD
-print("Making changes here only, it wont relfect in test-branch")
-
-=======
 print("Now you are in test-branch, changes wont reflect in master!")
->>>>>>> test-branch
